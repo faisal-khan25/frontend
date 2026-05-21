f378d764fb660d7f35cc1fcd52fb07602d3d1439
faisal <pathanfaisalkhan313@gmail.com>
2026-05-21 23:19:01 +0530

    changed the url to local

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
-const BACKEND_URL = import.meta.env.VITE_API_URL || 'https://new-backend-job-portal.onrender.com';
+//  const BACKEND_URL = import.meta.env.VITE_API_URL='http://localhost:8080'
+// //  'https://new-backend-job-portal.onrender.com';
+
+const BACKEND_URL =
+  import.meta.env.VITE_API_URL || "http://localhost:8080";
+
+console.log(BACKEND_URL);
 
 const api = axios.create({
   baseURL: BACKEND_URL,
