df9de0b762f6ffc893fa56c579ad84d666f9bc24
Faisal khan <pathanfaisalkhan313@gmail.com>
2026-05-22 11:25:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,32 +6,32 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-//   build: {
-//     outDir: 'dist',
-//     rollupOptions: {
-//       output: {
-//         manualChunks: undefined,
-//       },
-//     },
-//   },
-// })
-build: {
-  minify: 'terser',
-terserOptions: { compress: { drop_console: true } },
-  outDir: 'dist',
-  rollupOptions: {
-    output: {
-      manualChunks(id) {
-        if (id.includes('node_modules')) {
-          if (id.includes('recharts')) return 'recharts';
-          if (id.includes('react-router')) return 'router';
-          if (id.includes('axios')) return 'axios';
-          if (id.includes('react')) return 'react';
-          return 'vendor';
-          
-        }
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
       },
     },
   },
-},
 })
+// build: {
+//   minify: 'terser',
+// terserOptions: { compress: { drop_console: true } },
+//   outDir: 'dist',
+//   rollupOptions: {
+//     output: {
+//       manualChunks(id) {
+//         if (id.includes('node_modules')) {
+//           if (id.includes('recharts')) return 'recharts';
+//           if (id.includes('react-router')) return 'router';
+//           if (id.includes('axios')) return 'axios';
+//           if (id.includes('react')) return 'react';
+//           return 'vendor';
+          
+//         }
+//       },
+//     },
+//   },
+// },
+// })
