867c3393df772c9a8b7d92918882066f691da84a
faisal <pathanfaisalkhan313@gmail.com>
2026-05-22 16:10:56 +0530

    chnages

--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { useAuth } from '../../context/AuthContext';
 
 export default function ProtectedRoute({ children, role }) {
   const { user } = useAuth();
-  if (!user) return <Navigate to="/login" />;
+  // if (!user) return <Navigate to="/login" />;
   if (role && user.role !== role) return <Navigate to="/" />;
   return children;
 }
